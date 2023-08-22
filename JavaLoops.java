af024e2b7cd89c18895e1e8b0ef68ac58f164717
yuvikadhiman <yuvikadhiman25@gmail.com>
2023-08-22 22:46:35 +0530

    .

--- JavaLoops.java
+++ JavaLoops.java
@@ -1,7 +1,9 @@
 public class JavaLoops {
     public static void main (String arg[]){
-        for(int i=5;i<=1;i--){
-            System.out.println("*"+i);
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.println("*");
+            }
         }
     }
 }
