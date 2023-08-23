111a55d3769c4b1db0c5588d629d4885d2020c0f
yuvikadhiman <yuvikadhiman25@gmail.com>
2023-08-23 16:30:58 +0530

    functions

--- JavaFunctions.java
+++ JavaFunctions.java
@@ -29,22 +29,46 @@
 
 // -----------CONVERT BINARY TO DECIMAL------------
 
+// import java.util.Scanner;
+// public class JavaFunctions {
+//     public static  void binToDec(int binNum){
+//         int myNum=binNum;
+//         int pow=0;
+//         int decNum=0; 
+//         while(binNum>0){
+//             int lastDigit=binNum%10;
+//             decNum= decNum+(lastDigit* (int)Math.pow(2,pow));
+//             pow++;
+//             binNum=binNum/10;
+//         }
+//         System.out.println("decimal of "+ myNum +" : " +decNum);
+//     }
+//     public static void main(String args[]){
+//         int num=11001;
+//         binToDec(num);
+        
+//     } 
+    
+// }
+
+// -----------CONVERT DECIMAL TO BINARY ------------
+
 import java.util.Scanner;
 public class JavaFunctions {
-    public static  void binToDec(int binNum){
-        int myNum=binNum;
+    public static  void binToDec(int decNum){
+        int myNum=decNum;
         int pow=0;
-        int decNum=0; 
-        while(binNum>0){
-            int lastDigit=binNum%10;
-            decNum= decNum+(lastDigit* (int)Math.pow(2,pow));
+        int binNum=0; 
+        while(decNum>0){
+            int rem= decNum%2;
+            binNum=binNum+(rem*(int)Math.pow(10,pow));
             pow++;
-            binNum=binNum/10;
+            decNum=decNum/2;
         }
-        System.out.println("decimal of "+ myNum +" : " +decNum);
+        System.out.println("Binary of "+ myNum +" : " +binNum);
     }
     public static void main(String args[]){
-        int num=11001;
+        int num=7;
         binToDec(num);
         
     } 
