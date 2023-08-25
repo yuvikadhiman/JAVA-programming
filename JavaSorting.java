fc6733284adb73db0c7b868ec6703f817a1bfdde
yuvikadhiman <yuvikadhiman25@gmail.com>
2023-08-26 01:44:32 +0530

    .

--- JavaSorting.java
+++ JavaSorting.java
@@ -25,29 +25,52 @@
   
 
 // --------selection sort--------------------
-public class JavaSorting {
+// public class JavaSorting {
 
-    public static void selectSort(int array[]){
-        int n=array.length;
-        for(int i=0;i<n-1;i++){
-            int min=i;
-            for(int j=i+1;j<n;j++){
-                if(array[min]>array[j]){
-                    min=j;
-                }
-            }
-            int temp=array[min];
-            array[min]=array[i];
-            array[i]=temp;    
-        }
-        for(int i=0;i<n;i++){
-            System.out.print(array[i]+" ");
-         }
-        }
-    public static void main(String args[]){
-        int array[]={5,4,1,3,2,6,8};
-        selectSort(array);
-
-    }
-}
+//     public static void selectSort(int array[]){
+//         int n=array.length;
+//         for(int i=0;i<n-1;i++){
+//             int min=i;
+//             for(int j=i+1;j<n;j++){
+//                 if(array[min]>array[j]){
+//                     min=j;
+//                 }
+//             }
+//             int temp=array[min];
+//             array[min]=array[i];
+//             array[i]=temp;    
+//         }
+//         for(int i=0;i<n;i++){
+//             System.out.print(array[i]+" ");
+//          }
+//         }
+//     public static void main(String args[]){
+//         int array[]={5,4,1,3,2,6,8};
+//         selectSort(array);
+//     }
+// }
   
+// --------------Insertion Sort--------------------
+
+// public class JavaSorting {
+//     public static void insertionSort(int array[]){
+//         for(int i=1;i<array.length;i++){
+//             int curr=array[i];
+//             int prev=i-1;
+//             while(prev>=0 && array[prev]>curr){
+//                array[prev+1]=array[prev];
+//                prev--;
+               
+//             }
+//             array[prev+1]=curr;
+//         }
+//          for(int i=0;i<array.length;i++){
+//             System.out.print(array[i]+" ");
+//          }
+//         }
+        
+//     public static void main(String args[]){
+//         int array[]={5,4,1,3,2};
+//         insertionSort(array);
+//     }
+// }
---
+++ JavaStrings.java
@@ -0,0 +1,6 @@
+public class JavaStrings {
+        public static void main (String args[]){
+        String str1=" dhiman yuvika myself hello";
+        System.out.println(str1.length());
+    }
+}
