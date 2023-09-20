e5c7a695362b5315d2c03ae1ac3e177cbba2e2a7
yuvikadhiman <yuvikadhiman25@gmail.com>
2023-09-20 10:22:31 +0530

    Update JavaGUI.java

--- JavaGUI.java
+++ JavaGUI.java
@@ -3,14 +3,34 @@ import java.awt.event.*;
 
 public class JavaGUI extends Frame implements ActionListener{
     
-    TextField textField;
+    TextField textField,textField2;
+    Label label;
 
     JavaGUI(){
+        Label label =new Label("Name");
+        label.setBounds(20,50,50,25);
+
         textField = new TextField();
-        textField.setBounds(60, 50, 200, 25);
+        textField.setBounds(100, 50, 200, 25);
+
+        Label label1 =new Label("Address");
+        label1.setBounds(20,100,50,25);
+
+        textField2 = new TextField();
+        textField2.setBounds(100, 100, 200, 25);
+
+        Label label2 =new Label("Gender");
+        label2.setBounds(20,150,50,25);
+
+        CheckboxGroup checkboxGroup=new CheckboxGroup();
+        Checkbox checkbox1=new Checkbox("Male",checkboxGroup,false);
+        Checkbox checkbox2=new Checkbox("Female",checkboxGroup,false);
+        checkbox1.setBounds(100,150,50,30);
+        checkbox2.setBounds(170,150,50,30);
+
         
-        Button button = new Button("click Here");
-        button.setBounds(100, 120, 80, 30);
+        Button button = new Button("Submit");
+        button.setBounds(100, 220, 80, 30);
         button.addActionListener(this);
 
         addWindowListener(new WindowAdapter() {
@@ -18,16 +38,28 @@ public class JavaGUI extends Frame implements ActionListener{
                 System.exit(0);
             }
         });
+        add(label);
+        add(label1);
+        add(label2);
         add(textField);
+        add(textField2);
+        add(checkbox1);
+        add(checkbox2);
+
+
         add(button);
         setSize(300, 300);
+        setMinimumSize(new Dimension(400, 400));
+        setMaximumSize(new Dimension(500, 500));
         setLayout(null);
         setVisible(true);
 
     }
     
     public void actionPerformed(ActionEvent e){ 
-        textField.setText("welcome");
+        textField.setText("");
+        textField2.setText("");
+
     }
     public static void main(String[] args)
     {
